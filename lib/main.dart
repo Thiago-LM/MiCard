dd5b8b6c55b4b5561c98ec966c7c135d20ebee3e
Thiago Leal Menezes <thiago.pixelado@gmail.com>
2021-02-13 12:35:37 -0300

    Change to personal business card.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-void main() {
-  runApp(
-    MyApp(),
-  );
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
@@ -13,40 +10,89 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            children: [
-              Container(
-                width: 100.0,
-                color: Colors.red,
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(
+              Icons.supervised_user_circle,
+              size: 100.0,
+            ),
+            Text(
+              'Thiago Leal Menezes',
+              style: TextStyle(
+                fontSize: 40.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Pacifico',
               ),
-              Spacer(
-                flex: 1,
+            ),
+            Text(
+              'FLUTTER DEVELOPER',
+              style: TextStyle(
+                fontSize: 20.0,
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+                color: Colors.teal.shade100,
+                fontFamily: 'Source Sans Pro',
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: 100.0,
-                    width: 100.0,
-                    color: Colors.yellow,
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 250.0,
+              child: Divider(
+                color: Colors.teal.shade100,
+              ),
+            ),
+            Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  onTap: () async {
+                    const phone = 'tel://+55(85)997128622';
+                    if (await canLaunch(phone)) {
+                      await launch(phone);
+                    } else {
+                      throw 'Could not launch $phone';
+                    }
+                  },
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
                   ),
-                  Container(
-                    height: 100.0,
-                    width: 100.0,
-                    color: Colors.yellow.withOpacity(0.5),
+                  title: Text(
+                    '+55 (85) 9 9712-8622',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                    ),
                   ),
-                ],
-              ),
-              Spacer(
-                flex: 1,
-              ),
-              Container(
-                width: 100.0,
-                color: Colors.blue,
-              ),
-            ],
-          ),
-        ),
+                )),
+            Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  onTap: () async {
+                    const email = 'mailto:thiago.pixelado@gmail.com?subject=NEW%20JOB&body=Hi,';
+                    if (await canLaunch(email)) {
+                      await launch(email);
+                    } else {
+                      throw 'Could not launch $email';
+                    }
+                  },
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'thiago.pixelado@gmail.com',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                    ),
+                  ),
+                ))
+          ],
+        )),
       ),
     );
   }
